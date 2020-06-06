4497bac9a1286e2e352a7701539f3c1914d0a5a9
julitolopez <63474396+julitolopez@users.noreply.github.com>
2020-06-06 10:25:11 -0300

    initial commit

---
+++ game.js
@@ -0,0 +1,79 @@
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  userClickedPattern = [];
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+  $("#level-title").text("Level " + level);
+  level++;
+}
+
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+})
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3")
+  audio.play()
+}
+
+function wrongSound() {
+  var wrong = new Audio("sounds/wrong.mp3")
+  wrong.play()
+}
+
+function animatePress(currentColor) {
+  $("." + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function animateWrong() {
+  $("body").addClass("game-over");
+  setTimeout(function() {
+    $("body").removeClass("game-over");
+  }, 200);
+  $("h1").text("Game Over, Press any key to restart");
+}
+
+
+$(document).keydown(function(){
+  if (started === false) {
+    nextSequence();
+    started = true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function(){
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    wrongSound();
+    animateWrong();
+    startOver();
+  }
+}
+
+function startOver() {
+  gamePattern = [];
+  started = false;
+  level = 0;
+}
